0769005b439a48b84eb3b904d797f3c606c77647
RamiroPerez07 <ramiperez1997@hotmail.com>
2023-02-09 17:54:55 -0300

    agregadas imagenes

--- src/data/casousel-img.js
+++ src/data/casousel-img.js
@@ -1,16 +1,16 @@
 export const slides = [
   {
-    img: "https://res.cloudinary.com/dhnicvwkw/image/upload/v1675969348/Perfarm/giza/jeroen-den-otter-2b0JeJTEclQ-unsplash_ahdcup.jpg",
+    img: "https://res.cloudinary.com/dhnicvwkw/image/upload/v1675969217/Perfarm/giza/vista-frontal-mujer-frasco-perfume_omdd2z.jpg",
     label: "Perfumes",
     description: "¡Encontrá la fragancia que buscás!",
   },
   {
-    img: "https://res.cloudinary.com/dhnicvwkw/image/upload/v1675968841/Perfarm/giza/pexels-valeria-boltneva-1961795_vidi0m.jpg",
+    img: "https://res.cloudinary.com/dhnicvwkw/image/upload/v1675976027/pexels-nur-andi-ravsanjani-gusma-5983409_h63ib5.jpg",
     label: "Desodorantes",
     description: "Antitranspirantes y Bodyspray",
   },
   {
-    img: "https://res.cloudinary.com/dhnicvwkw/image/upload/v1675968811/Perfarm/giza/pexels-cottonbro-studio-4620874_ywiypr.jpg",
+    img: "https://res.cloudinary.com/dhnicvwkw/image/upload/v1675975914/Perfarm/giza/pexels-cottonbro-studio-4620874_qf3bjz.jpg",
     label: "Maquillaje",
     description: "Variedad y precio",
   },
