fca44913835ae0c76fa04801cdeee02888137fb8
RamiroPerez07 <ramiperez1997@hotmail.com>
2023-02-10 18:13:08 -0300

    agregado ventana de filtros

--- src/components/FilterProduct/FilterProduct.jsx
+++ src/components/FilterProduct/FilterProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { 
     Modal,
     ModalOverlay,
@@ -15,12 +15,18 @@ import {
     RangeSliderTrack,
     RangeSliderThumb,
     RangeSliderFilledTrack, 
+    Text,
+    useColorModeValue,
 } from '@chakra-ui/react';
+import { colorPalette } from '../../styles/colors';
 
 
 export const FilterProduct = (props) => {
 
   const {isOpen,onClose, initialRef, finalRef} = props;
+
+  //llamo al estado para el manejador de rango de precio
+  const [price, setPrice] = useState({inf: 0, sup: 100000})
 
   return (
     <>
@@ -37,16 +43,25 @@ export const FilterProduct = (props) => {
           <ModalBody pb={6}>
             <FormControl>
               <FormLabel>Categoria</FormLabel>
-              <Select ref={initialRef} placeholder='Seleccionar Categoría'>
+              <Select ref={initialRef} placeholder='Seleccionar categoría' focusBorderColor={useColorModeValue(colorPalette.light.terciary, colorPalette.dark.terciary)}>
                 <option value='option1'>Perfumes</option>
                 <option value='option2'>Aerosoles</option>
                 <option value='option3'>Maquillaje</option>
               </Select>
             </FormControl>
 
+            <FormControl>
+              <FormLabel>Envío</FormLabel>
+              <Select ref={initialRef} placeholder='Seleccionar opción' focusBorderColor={useColorModeValue(colorPalette.light.terciary, colorPalette.dark.terciary)}>
+                <option value='option1'>Gratuito</option>
+                <option value='option2'>Envío con cargo</option>
+              </Select>
+            </FormControl>
+
             <FormControl mt={4}>
               <FormLabel>Precio</FormLabel>
-              <RangeSlider aria-label={['min', 'max']} defaultValue={[10, 30]}>
+              <Text mb="10px">{`Entre $${price.inf} y $${price.sup}`}</Text>
+              <RangeSlider colorScheme={colorPalette.chakraScheme.button} aria-label={['min', 'max']} defaultValue={[0, 100000]} min={0} max={100000} step={5000} onChange={(value)=>setPrice({inf:value[0], sup:value[1]})}>
                 <RangeSliderTrack>
                   <RangeSliderFilledTrack />
                 </RangeSliderTrack>
