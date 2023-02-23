a59b0cd5354a0ef4b4feceb78e0a7132165aae19
RamiroPerez07 <ramiperez1997@hotmail.com>
2023-02-23 00:09:44 -0300

    agregado clck en imagen

--- src/components/HeaderContent/HeaderContent.jsx
+++ src/components/HeaderContent/HeaderContent.jsx
@@ -9,6 +9,7 @@ import {SunIcon, MoonIcon} from '@chakra-ui/icons';
 import { colorPalette } from '../../styles/colors';
 import { ShoppingCart } from '../ShoppingCart/ShoppingCart';
 import { useDisclosure } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 
 export const HeaderContent = () => {
@@ -21,9 +22,12 @@ export const HeaderContent = () => {
   //gestion de la vista del carrito
   const { isOpen, onOpen, onClose } = useDisclosure()
 
+  //llamo al administrador de navegacion
+  const navigate = useNavigate();
+
   return (
     <StyledHeaderContent bg={useColorModeValue(colorPalette.light.primary,colorPalette.dark.primary)}>
-      <StyledImg src={imgGiza} alt="Giza" />
+      <StyledImg src={imgGiza} alt="Giza" onClick={()=> navigate("/")} />
       <Spacer />
       <StyledMenuOptions>
         <IconButton icon={colorMode === "light" ? <MoonIcon /> : <SunIcon />} aria-label='Cambiar Modo' onClick={toggleColorMode}></IconButton>
